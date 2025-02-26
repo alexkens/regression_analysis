a8faed850943321e3194a552d5a3a444427984ed
andy_wendy <alexkens93@gmail.com>
2025-02-26 18:33:36 +0100

    init

---
+++ main.py
@@ -0,0 +1,82 @@
+from statistics import mean
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# salary data: https://www.kaggle.com/datasets/abhishek14398/salary-dataset-simple-linear-regression/data
+PATH = "salary_dataset.csv"
+
+def simple_linear_regression():
+    df = pd.read_csv(PATH)
+
+    mu_x = df["YearsExperience"].mean()
+    mu_y = df["Salary"].mean()
+    x = df["YearsExperience"].values
+    y = df["Salary"].values
+
+    numerator = sum([(xi - mu_x) * (yi - mu_y) for xi, yi in zip(x, y)])
+    denominator = sum([(xi - mu_x) ** 2 for xi in x])
+    beta = numerator / denominator
+    alpha = mu_y - beta * mu_x
+
+    return alpha, beta
+
+
+def plot_residuals_by_predicted(alpha, beta):
+    df = pd.read_csv(PATH)
+    x = df["YearsExperience"]
+    y = df["Salary"]
+    residuals = [abs(alpha + beta * xi - yi) for xi, yi in zip(x, y)]
+
+    plt.scatter(y, residuals)
+    plt.axhline(mean(residuals), color='red', linestyle="--")
+
+    # style
+    plt.title("Residuals by Predicted")
+    plt.xlabel("Salary")
+    plt.ylabel("Residuals")
+
+    plt.show()
+
+
+def plot(alpha, beta):
+    df = pd.read_csv(PATH)
+
+    # data
+    plt.scatter(df["YearsExperience"], df["Salary"])
+    
+    # line
+    x1 = df["YearsExperience"].values[0]
+    x2 = df["YearsExperience"].values[-1]
+    y1 = alpha + beta * x1
+    y2 = alpha + beta * x2
+    plt.plot([x1, x2], [y1, y2], c="g")
+
+    # residuals
+    x = df["YearsExperience"]
+    y = df["Salary"]
+    f = [alpha + beta * xi for xi in x]
+    residuals = [abs(alpha + beta * xi - yi) for xi, yi in zip(x, y)]
+    plt.scatter(x, y, color="#1f77b4")
+
+    for xi, yi, ri, fi in zip(x, y, residuals, f):
+        if fi > yi:
+            ymin = yi
+            ymax = fi
+        else: # fi < yi
+            ymin = fi
+            ymax = yi
+        plt.vlines(x=xi, ymin=ymin, ymax=ymax, colors='red', linestyles='--', lw=2)
+        # plt.text(xi + 0.1, ((ymax - ymin) / 2) + ymin, s=f"{ri:.1f}", horizontalalignment='left')
+
+    # style
+    plt.title("Simple Linear Regression")
+    plt.xlabel("Years of Experience")
+    plt.ylabel("Salary")
+    
+    plt.show()
+
+
+alpha, beta = simple_linear_regression()
+print(alpha, beta)
+plot_residuals_by_predicted(alpha, beta)
