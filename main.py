87c80a8a477bc565eac3208573b5a39ae6373ec4
andy_wendy <alexkens93@gmail.com>
2025-03-16 00:43:07 +0100

    new data set, improve code

--- main.py
+++ main.py
@@ -4,31 +4,30 @@ import matplotlib.pyplot as plt
 
 
 # salary data: https://www.kaggle.com/datasets/abhishek14398/salary-dataset-simple-linear-regression/data
-DATA = "data/salary_dataset.csv"
+DATA_SALARY = "data/salary_dataset.csv"
+DATA_INSURANCE = "data/insurance.csv"
+DATA_ADS = "data/advertising.csv"
 TRIVIAL = "data/trivial_case.csv"
-PATH = DATA
+#PATH = DATA
 
-def simple_linear_regression():
-    df = pd.read_csv(PATH)
+def simple_linear_regression(path, ind_var, dep_var):
+    df = pd.read_csv(path)
 
-    mu_x = df["YearsExperience"].mean()
-    mu_y = df["Salary"].mean()
-    x = df["YearsExperience"].values
-    y = df["Salary"].values
+    mu_x = df[ind_var].mean()
+    mu_y = df[dep_var].mean()
+    x = df[ind_var].values
+    y = df[dep_var].values
 
     numerator = sum([(xi - mu_x) * (yi - mu_y) for xi, yi in zip(x, y)])
     denominator = sum([(xi - mu_x) ** 2 for xi in x])
     beta = numerator / denominator
     alpha = mu_y - beta * mu_x
 
-    U = [round(xi * beta + alpha, 2) for xi in x]
-    print(U)
-
     return alpha, beta
 
 
-def plot_residuals_by_predicted(alpha, beta):
-    df = pd.read_csv(PATH)
+def plot_residuals_by_predicted(alpha, beta, path):
+    df = pd.read_csv(path)
     x = df["YearsExperience"]
     y = df["Salary"]
     residuals = [abs(alpha + beta * xi - yi) for xi, yi in zip(x, y)]
@@ -44,54 +43,68 @@ def plot_residuals_by_predicted(alpha, beta):
     plt.show()
 
 
-def plot(alpha, beta):
-    df = pd.read_csv(PATH)
-
+def plot(path, ind_var, dep_var, residual_flag=False, save_flag=False):
     # data
-    plt.scatter(df["YearsExperience"], df["Salary"])
+    alpha, beta = simple_linear_regression(path, ind_var, dep_var)
+    df = pd.read_csv(path)
+    plt.scatter(df[ind_var], df[dep_var])
     
     # line
-    x1 = df["YearsExperience"].values[0]
-    x2 = df["YearsExperience"].values[-1]
+    x1 = df[ind_var].values[0]
+    x2 = df[ind_var].values[-1]
     y1 = alpha + beta * x1
     y2 = alpha + beta * x2
     plt.plot([x1, x2], [y1, y2], c="g")
 
     # residuals
-    x = df["YearsExperience"]
-    y = df["Salary"]
-    f = [alpha + beta * xi for xi in x]
-    residuals = [abs(alpha + beta * xi - yi) for xi, yi in zip(x, y)]
-    plt.scatter(x, y, color="#1f77b4")
+    if residual_flag:
+        x = df[ind_var]
+        y = df[dep_var]
+        f = [alpha + beta * xi for xi in x]
+        residuals = [abs(alpha + beta * xi - yi) for xi, yi in zip(x, y)]
+        plt.scatter(x, y, color="#1f77b4")
 
-    for xi, yi, ri, fi in zip(x, y, residuals, f):
-        if fi > yi:
-            ymin = yi
-            ymax = fi
-        else: # fi < yi
-            ymin = fi
-            ymax = yi
-        plt.vlines(x=xi, ymin=ymin, ymax=ymax, colors='red', linestyles='--', lw=2)
-        # plt.text(xi + 0.1, ((ymax - ymin) / 2) + ymin, s=f"{ri:.1f}", horizontalalignment='left')
+        for xi, yi, ri, fi in zip(x, y, residuals, f):
+            if fi > yi:
+                ymin = yi
+                ymax = fi
+            else: # fi < yi
+                ymin = fi
+                ymax = yi
+            plt.vlines(x=xi, ymin=ymin, ymax=ymax, colors='red', linestyles='--', lw=2)
+            # plt.text(xi + 0.1, ((ymax - ymin) / 2) + ymin, s=f"{ri:.1f}", horizontalalignment='left')
 
     # style
     plt.title("Simple Linear Regression")
-    plt.xlabel("Years of Experience")
-    plt.ylabel("Salary")
-    #plt.savefig("plots/residuals.png")
-    plt.show()
+    plt.xlabel(ind_var)
+    plt.ylabel(dep_var)
+    if save_flag:
+        s = path
+        print(s)
+        if ".csv" in s:
+            s = s.replace("data/", "").replace(".csv", "")
+        plt.savefig(s, format="png")
+    else:
+        plt.show()
+    plt.close()
+    
 
 
-alpha, beta = simple_linear_regression()
-print(alpha, beta)
-plot(alpha, beta)
+def get_squared_error(path, ind_var, dep_var):
+    alpha, beta = simple_linear_regression(path, ind_var, dep_var)
+    df = pd.read_csv(path)
 
-"""df = pd.read_csv("perfect_line.csv")
-plt.scatter(df["YearsExperience"], df["Salary"])
-plt.title("Simple Linear Regression")
-plt.xlabel("Years of Experience")
-plt.ylabel("Salary")
-plt.savefig("plots/perfect_line.png")"""
+    X = df[ind_var].values
+    Y = df[dep_var].values
 
-#plot(alpha, beta)
-#plot_residuals_by_predicted(alpha, beta)
+    U = [((xi * beta + alpha) - yi) ** 2 for xi, yi in zip(X, Y)]
+    return sum(U)
+
+
+error1 = get_squared_error(DATA_SALARY, "YearsExperience", "Salary")
+error2 = get_squared_error(DATA_INSURANCE, "age", "charges")
+print("{:.2f}, {:.2f}".format(error1, error2))
+
+# plot(DATA_SALARY, "YearsExperience", "Salary", save_flag=True)
+# plot(DATA_INSURANCE, "age", "charges", save_flag=False)
+plot(DATA_INSURANCE, "age", "charges", save_flag=False)
